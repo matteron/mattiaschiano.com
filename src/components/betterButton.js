27385b26ed5d9cfca63c49d2ad9316919096b6b4
matteron <matteron96@gmail.com>
2018-07-30 09:27:43 -0400

    Fixed FOUC on button press

--- src/components/betterButton.js
+++ src/components/betterButton.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 import styles from '../styles/betterButton.module.css';
 
 const BetterButton = ({text, link}) => (
-    <a href={link} className={styles.buttonFace}>
+    <Link to={link} className={styles.buttonFace}>
         <p className={styles.text}>{text}</p>
-    </a>
+    </Link>
 )
 
 export default BetterButton
